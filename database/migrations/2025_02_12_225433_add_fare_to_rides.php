297cc9d178382578ea3256e367b1c89a25f281cd
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-02-18 15:34:11 -0400

    Fix/ajustes 1 (#9)
    * add Job's name
    
    * fix column not exists
    
    * add php postgresql ext

--- database/migrations/2025_02_12_225433_add_fare_to_rides.php
+++ database/migrations/2025_02_12_225433_add_fare_to_rides.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('rides', function (Blueprint $table) {
-            $table->unsignedBigInteger('fare')->default(0)->after('distance');
+            $table->unsignedBigInteger('fare')->default(0)->after('status');
         });
     }
 
