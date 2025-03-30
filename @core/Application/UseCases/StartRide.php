482ffc6a0b04d70dece6d40af4282993c8b94c44
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-03-29 23:10:27 -0400

    using message-broker (#22)
    * using message-broker
    
    * style: apply pint
    
    * ci: apply deploy only push on main

--- @core/Application/UseCases/StartRide.php
+++ @core/Application/UseCases/StartRide.php
@@ -2,17 +2,19 @@
 
 namespace Core\Application\UseCases;
 
+use App\Services\MessageBroker\MessageBroker;
+use App\Services\UnitOfWork\UnitOfWork;
 use Core\Application\Repositories\RideRepository;
 use Core\Application\UseCases\DTOs\StartRideInput;
-use Core\Domain\Events\EventDispatcher;
 use Core\Domain\Events\RideStartedEvent;
 use Core\Domain\Exceptions\RideNotFoundException;
 
 class StartRide
 {
     public function __construct(
+        private readonly UnitOfWork $unitOfWork,
+        private readonly MessageBroker $messageBroker,
         private readonly RideRepository $rideRepository,
-        private readonly EventDispatcher $eventDispatcher
     ) {}
 
     public function execute(StartRideInput $input): void
@@ -22,9 +24,16 @@ class StartRide
             throw new RideNotFoundException;
         }
 
-        $ride->register(RideStartedEvent::name(), function ($event) use ($ride) {
-            $this->rideRepository->update($ride);
-            $this->eventDispatcher->dispatch($event);
+        $ride->register(RideStartedEvent::name(), function (RideStartedEvent $event) use ($ride) {
+            try {
+                $this->rideRepository->update($ride);
+
+                $this->messageBroker->publish($event->getName(), $event->getData());
+                $this->unitOfWork->commit();
+            } catch (\Throwable $th) {
+                $this->unitOfWork->rollback();
+                throw $th;
+            }
         });
 
         $ride->start();
--- tests/Integration/StartRideTest.php
+++ tests/Integration/StartRideTest.php
@@ -1,69 +1,101 @@
 <?php
 
 use App\Account as AccountModel;
-use App\Position as PositionModel;
-use App\Repositories\AccountModelRepository;
-use App\Repositories\PositionModelRepository;
 use App\Repositories\RideModelRepository;
 use App\Ride as RideModel;
+use App\Services\MessageBroker\MessageBroker;
+use App\Services\UnitOfWork\DatabaseUnitOfWork;
+use App\Services\UnitOfWork\UnitOfWork;
+use Core\Application\Repositories\PositionRepository;
+use Core\Application\Repositories\RideRepository;
 use Core\Application\UseCases\DTOs\GetRideInput;
 use Core\Application\UseCases\DTOs\GetRideOutput;
 use Core\Application\UseCases\DTOs\StartRideInput;
 use Core\Application\UseCases\GetRide;
-use Core\Application\UseCases\Signup;
 use Core\Application\UseCases\StartRide;
-use Core\Domain\Events\EventDispatcher;
+use Core\Domain\Entities\Ride;
 use Core\Domain\Exceptions\RideCannotBeStartedException;
 use Core\Domain\Exceptions\RideNotFoundException;
 use Core\Domain\ValueObjects\Uuid;
 
-beforeEach(function () {
-    $accountRepository = new AccountModelRepository(new AccountModel);
-    $this->signup = new Signup(accountRepository: $accountRepository);
-
-    $eventDispatcher = new EventDispatcher;
-    $rideRepository = new RideModelRepository(new RideModel);
-    $this->startRide = new StartRide(
-        rideRepository: $rideRepository,
-        eventDispatcher: $eventDispatcher
-    );
-
-    $positionRepository = new PositionModelRepository(new PositionModel);
-    $this->getRide = new GetRide(
-        rideRepository: $rideRepository,
-        positionRepository: $positionRepository
-    );
-});
-
 describe('StartRide', function () {
     test('Deve falhar não encontrar a corrida', function () {
+        $unitOfWork = Mockery::mock(UnitOfWork::class);
+        $unitOfWork->shouldReceive('commit')->times(0);
+        $unitOfWork->shouldReceive('rollback')->times(0);
+        $messageBroker = Mockery::mock(MessageBroker::class);
+        $messageBroker->shouldReceive('publish')->times(0);
+        $rideRepository = Mockery::mock(RideRepository::class);
+        $rideRepository->shouldReceive('getById')->andReturnNull();
+        $startRide = new StartRide(
+            unitOfWork: $unitOfWork,
+            messageBroker: $messageBroker,
+            rideRepository: $rideRepository,
+        );
         $startRideInput = new StartRideInput(
             rideId: Uuid::create(),
         );
 
-        expect(fn () => $this->startRide->execute($startRideInput))->toThrow(RideNotFoundException::class);
+        expect(fn () => $startRide->execute($startRideInput))->toThrow(RideNotFoundException::class);
     });
 
     test('Deve falhar ao iniciar uma corrida que não foi aceita', function () {
-        $passengerModel = AccountModel::factory()->passenger()->create();
-        $rideModel = RideModel::factory()->requested()
-            ->create(['passenger_id' => $passengerModel->account_id]);
+        $unitOfWork = Mockery::mock(UnitOfWork::class);
+        $unitOfWork->shouldReceive('commit')->times(0);
+        $unitOfWork->shouldReceive('rollback')->times(0);
+        $messageBroker = Mockery::mock(MessageBroker::class);
+        $messageBroker->shouldReceive('publish')->times(0);
+        $rideRepository = Mockery::mock(RideRepository::class);
 
-        $startRideInput = new StartRideInput($rideModel->ride_id);
-        expect(fn () => $this->startRide->execute($startRideInput))->toThrow(RideCannotBeStartedException::class);
+        $ride = new Ride(
+            rideId: (string) Uuid::create(),
+            passengerId: (string) Uuid::create(),
+            status: 'requested',
+            fromLatitude: -15,
+            fromLongitude: 56,
+            toLatitude: -15,
+            toLongitude: 56,
+        );
+
+        $rideRepository->shouldReceive('getById')->andReturn($ride);
+        $startRide = new StartRide(
+            unitOfWork: $unitOfWork,
+            messageBroker: $messageBroker,
+            rideRepository: $rideRepository,
+        );
+
+        $startRideInput = new StartRideInput($ride->getId());
+        expect(fn () => $startRide->execute($startRideInput))->toThrow(RideCannotBeStartedException::class);
     });
 
     test('Deve iniciar uma corrida', function () {
+
         $passengerModel = AccountModel::factory()->passenger()->create();
         $driverModel = AccountModel::factory()->driver()->create();
         $rideModel = RideModel::factory()->accepted($driverModel->account_id)
             ->create(['passenger_id' => $passengerModel->account_id]);
 
-        $startRideInput = new StartRideInput($rideModel->ride_id);
-        $this->startRide->execute($startRideInput);
+        $unitOfWork = new DatabaseUnitOfWork;
+        $messageBroker = Mockery::mock(MessageBroker::class);
+        $messageBroker->shouldReceive('publish')->times(1);
+        $rideRepository = new RideModelRepository(new RideModel);
+        $startRide = new StartRide(
+            unitOfWork: $unitOfWork,
+            messageBroker: $messageBroker,
+            rideRepository: $rideRepository,
+        );
 
+        $startRideInput = new StartRideInput($rideModel->ride_id);
+        $startRide->execute($startRideInput);
+
+        $positionRepository = Mockery::mock(PositionRepository::class);
+        $positionRepository->shouldReceive('getPositionsByRideId')->andReturn([]);
+        $getRide = new GetRide(
+            rideRepository: $rideRepository,
+            positionRepository: $positionRepository
+        );
         $getRideInput = new GetRideInput($rideModel->ride_id);
-        $getRideOutput = $this->getRide->execute($getRideInput);
+        $getRideOutput = $getRide->execute($getRideInput);
 
         expect($getRideOutput)->toBeInstanceOf(GetRideOutput::class);
         expect($getRideOutput->status)->toBe('in_progress');
